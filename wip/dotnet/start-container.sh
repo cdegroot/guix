b10b1cc7fa4d9c4a8963724e0c6ba2c6f0e94795
Cees de Groot <casedeg@gmail.com>
2021-03-21 11:37:19 -0400

    Fix container start script

--- wip/dotnet/start-container.sh
+++ wip/dotnet/start-container.sh
@@ -1,14 +1,15 @@
 set -e
-cd ~/OpenSource/dotnet-source-build
+scriptdir=$(cd $(dirname $0); pwd)
+cd $scriptdir/source-build
 rm -f .guix-gc-root
-guix environment --container --network --root=.guix-gc-root \
+guix environment --container --network --link-profile \
+    --root=.guix-gc-root \
     --pure \
     --expose=$GUIX_PROFILE/bin/env=/usr/bin/env \
+    --expose=$GUIX_PROFILE/bin/bash=/bin/bash \
+    --expose=$scriptdir=/dotnet-build \
     --ad-hoc -e '(@ (gnu) %base-packages))' \
-              git curl openssl wget nss-certs patchelf gcc-toolchain \
+              guix \
+              cmake make clang python glibc-utf8-locales git curl bash openssl wget nss-certs patchelf binutils \
+              icu4c libunwind liburcu lttng-ust linux-libre-headers \
     -- bash
-
-# Notes. First, install .dotnet.
-# eng/common/dotnet-install.sh
-# Then, patch dotnet executable
-#
