59fcb59d41831af9f8cfe6a8702a8e31727fdc19
Cees de Groot <casedeg@gmail.com>
2021-03-27 12:05:43 -0400

    Fix module name

--- channel/cdg/dotnet.scm
+++ channel/cdg/dotnet.scm
@@ -16,7 +16,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (nongnu packages dotnet)
+(define-module (cdg dotnet)
   #:use-module (ice-9 match)
   #:use-module (guix download)
   #:use-module ((guix licenses) #:prefix license:)
