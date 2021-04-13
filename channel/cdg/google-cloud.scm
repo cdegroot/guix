8b00c220d6da605f1727d5a960ee7ea457800a4a
Cees de Groot <cg@evrl.com>
2021-04-13 11:58:46 -0400

    Add kubectl

--- channel/cdg/google-cloud.scm
+++ channel/cdg/google-cloud.scm
@@ -12,11 +12,11 @@
    (source
     (origin
      (method url-fetch/tarbomb)
-        (uri (string-append "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-"
-                            version
-                            "-linux-x86_64.tar.gz"))
-        (sha256
-         (base32 "12vbzi863x9p611548q0j7lnp0f2mjnp86sam21w63f77dv39hlw"))))
+     (uri (string-append "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-"
+                         version
+                         "-linux-x86_64.tar.gz"))
+     (sha256
+      (base32 "12vbzi863x9p611548q0j7lnp0f2mjnp86sam21w63f77dv39hlw"))))
    (build-system copy-build-system)
    (arguments
     '(#:install-plan '(("google-cloud-sdk/" "/" #:exclude-regexp ("/test/" "/tests/")))))
@@ -26,3 +26,30 @@
    (description "Google Cloud SDK, including the cloud CLI")
    (license license:asl2.0)
    (home-page "https://https://cloud.google.com/sdk/docs/install")))
+
+(define-public kubectl
+  (package
+    (name "kubectl")
+    (version "v1.12.9")
+    (source
+     (origin
+       (method url-fetch/executable)
+       (uri (string-append "https://dl.k8s.io/release/"
+                           version
+                           "/bin/linux/amd64/kubectl"))
+       (file-name "kubectl")
+       (sha256
+        (base32 "07sgj5qj0xp6jrasqz2q1abgabb4mb4d3wcril747d65hfjyz82k"))))
+    (build-system copy-build-system)
+    (arguments
+     '(#:install-plan '(("kubectl" "bin/"))
+       #:phases (modify-phases %standard-phases
+                  (replace 'unpack (lambda* (#:key source #:allow-other-keys)
+                                      (invoke "cp" source "kubectl")
+                                      #t)))))
+    (inputs '())
+    (synopsis "Kubernetes kubectl")
+    (description "The kubectl command line utility from Kubernetes")
+    (license license:asl2.0)
+    (home-page "https://github.com/kubernetes/kubernetes/")
+  ))
