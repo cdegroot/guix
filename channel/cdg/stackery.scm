f12de68cc0707ee2a91f7c432a964f08049f090b
Cees de Groot <cg@evrl.com>
2021-04-21 10:20:01 -0400

    Upgrade Stackery

--- channel/cdg/stackery.scm
+++ channel/cdg/stackery.scm
@@ -9,14 +9,14 @@
 (define-public stackery
   (package
     (name "stackery")
-    (version "5.0.2")
+    (version "5.1.0")
     (source
      (origin
        (method url-fetch/executable)
        (uri (string-append "https://ga.cli.stackery.io/linux/stackery"))
        (file-name "stackery.gz")
        (sha256
-        (base32 "0samjfj2y6hhv6xvf8fc1lbb32f4p149rs0df87scdav29k2gw4g"))))
+        (base32 "14345qn0qi3vladjjzk3pfr1hcc29b5hxj5ywxcrqk0rdfkdl9ja"))))
     (build-system copy-build-system)
     (arguments
      '(#:install-plan '(("stackery" "bin/"))
